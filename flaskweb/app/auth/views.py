a587cc0e93c21001f0ba2104cb1e8a364bf3f3d2
Gavin <q414625852@163.com>
2017-02-04 16:17:52 +0800

    f

--- flaskweb/app/auth/views.py
+++ flaskweb/app/auth/views.py
@@ -1,10 +1,18 @@
 # coding=utf-8
 
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
 from . import auth
 from .forms import LoginForm
+from flask_login import login_user
+from ..models import User
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('as！')
     return render_template('auth/login.html', form=form)
